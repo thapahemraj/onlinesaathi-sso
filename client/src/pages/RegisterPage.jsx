26785795a6bc6fc36e1f2a3ff0652635401b91ab
thapahemraj <thapahemraj204@gmail.com>
2026-02-07 18:13:28 +0530

    fix ragister page

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useAuth } from '../context/AuthContext';
 import { useNavigate, Link, useLocation } from 'react-router-dom';
 import { ArrowLeft, Eye, EyeOff } from 'lucide-react';
